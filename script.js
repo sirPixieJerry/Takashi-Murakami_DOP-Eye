f546ac88561226497ed73cfc7b05b63d78bc2b7d
CryptedJerry <jerry.bucquet@gmail.com>
2022-07-19 00:53:22 +0200

    handle elements refactor

--- script.js
+++ script.js
@@ -1,98 +1,102 @@
 (function () {
-    const lashOne = document.getElementById("lashes");
-    const lashTwo = document.querySelector("path");
-    const reflexOne = document.getElementById("reflexOne");
-    const reflexTwo = document.getElementById("reflexTwo");
+    // access elements with DOM
+    const box = document.getElementById("box");
+    const path = document.querySelector("path");
+    const lOne = document.getElementById("linkOne");
+    const lTwo = document.getElementById("linkTwo");
 
-    var arrItm = [];
-    var arrCol = [];
+    // setup clock
+    const steps = [];
+    let time;
+    let back;
 
-    var time;
-    var back;
-
-    const colorSpace = "rgb";
-
-    document.addEventListener("click", function (event) {
-        if (!event.target.matches(".lense")) return;
-
-        if (event.target.id === "lashes" || event.target.id === "lash") {
-            let clr2 = bgClr();
-
-            arrItm.push(lashOne.id);
-            arrCol.push(lashOne.style.backgroundColor);
-
-            lashOne.style.backgroundColor = clr2;
-            lashTwo.style.fill = clr2;
-        }
-        if (
-            event.target.id === "reflexOne" ||
-            event.target.id === "reflexTwo"
-        ) {
-            let clr = bgClr();
-
-            arrItm.push(reflexOne.id);
-            arrCol.push(reflexOne.style.backgroundColor);
-
-            reflexOne.style.backgroundColor = clr;
-            reflexTwo.style.backgroundColor = clr;
-        } else {
-            // push the recent color and object id into the array
-            arrItm.push(event.target.id);
-            arrCol.push(event.target.style.backgroundColor);
-            // change the actual color
-            event.target.style.backgroundColor = bgClr();
-        }
-        // resets the idle time of the rewind
-        document.onmousedown = resetTimer;
-        // the actual resetter of the idle time and rewind starter
-        function resetTimer() {
-            clearTimeout(time);
-            clearTimeout(back);
-            time = setTimeout(bckWrds, 5000);
-        }
-        // introducing the rewind
-        function bckWrds() {
-            // stops the rewind if arrays are empty
-            if (arrItm.length >= 1) {
-                // calls objects by the id stored in the arrays
-                var elm = document.getElementById(arrItm[arrItm.length - 1]);
-                // rewind for the lashes
-                if (elm.id === lashOne.id) {
-                    lashOne.style.backgroundColor = arrCol[arrCol.length - 1];
-                    lashTwo.style.fill = arrCol[arrCol.length - 1];
-                    // remove indexes in the arrays
-                    arrItm.pop();
-                    arrCol.pop();
-                    // calls function again
-                    back = setTimeout(bckWrds, 50);
-                    // rewind for the reflections
-                } else if (elm.id === reflexOne.id) {
-                    reflexOne.style.backgroundColor = arrCol[arrCol.length - 1];
-                    reflexTwo.style.backgroundColor = arrCol[arrCol.length - 1];
-                    // remove indexes in the arrays
-                    arrItm.pop();
-                    arrCol.pop();
-                    // calls function again
-                    back = setTimeout(bckWrds, 50);
-                    // rewind for the lenses
-                } else {
-                    elm.style.backgroundColor = arrCol[arrCol.length - 1];
-                    // remove indexes in the arrays
-                    arrItm.pop();
-                    arrCol.pop();
-                    // calls function again
-                    back = setTimeout(bckWrds, 50);
-                }
-            }
-        }
-    });
-
+    // generate rgb sring
     function bgClr() {
         const clr = Array(3)
             .fill()
             .map((i) => (i = (Math.random() * 255) | 0));
+        return `rgb(${clr[0]}, ${clr[1]}, ${clr[2]})`;
+    }
 
-        return `${colorSpace}(${clr[0]}, ${clr[1]}, ${clr[2]})`;
+    function resetTimer() {
+        clearTimeout(time);
+        clearTimeout(back);
+        time = setTimeout(bckWrds, 5000);
+    }
+
+    // event-listener setup
+    document.addEventListener("click", function (evt) {
+        // store current rgb string
+        const clr = bgClr();
+        // handle clickable elements
+        if (!evt.target.matches(".lense")) return;
+        resetTimer();
+        console.log(time);
+        if (evt.target.id === "box" || evt.target.id === "path") {
+            steps.push({
+                item: box.id,
+                color: box.style.backgroundColor,
+            });
+            box.style.backgroundColor = clr;
+            path.style.fill = clr;
+        } else if (evt.target.id === "linkOne" || evt.target.id === "linkTwo") {
+            steps.push({
+                item: lOne.id,
+                color: lOne.style.backgroundColor,
+            });
+            lOne.style.backgroundColor = clr;
+            lTwo.style.backgroundColor = clr;
+        } else {
+            steps.push({
+                item: evt.target.id,
+                color: evt.target.style.backgroundColor,
+            });
+            evt.target.style.backgroundColor = bgClr();
+        }
+
+        // resets the idle time of the rewind
+        // document.onmousedown = resetTimer;
+        // // the actual resetter of the idle time and rewind starter
+
+        // introducing the rewind
+        // function bckWrds() {
+        //     // stops the rewind if arrays are empty
+        //     if (arrItm.length >= 1) {
+        //         // calls objects by the id stored in the arrays
+        //         var elm = document.getElementById(arrItm[arrItm.length - 1]);
+        //         // rewind for the lashes
+        //         if (elm.id === lashOne.id) {
+        //             lashOne.style.backgroundColor = arrCol[arrCol.length - 1];
+        //             lashTwo.style.fill = arrCol[arrCol.length - 1];
+        //             // remove indexes in the arrays
+        //             arrItm.pop();
+        //             arrCol.pop();
+        //             // calls function again
+        //             back = setTimeout(bckWrds, 50);
+        //             // rewind for the reflections
+        //         } else if (elm.id === reflexOne.id) {
+        //             reflexOne.style.backgroundColor = arrCol[arrCol.length - 1];
+        //             reflexTwo.style.backgroundColor = arrCol[arrCol.length - 1];
+        //             // remove indexes in the arrays
+        //             arrItm.pop();
+        //             arrCol.pop();
+        //             // calls function again
+        //             back = setTimeout(bckWrds, 50);
+        //             // rewind for the lenses
+        //         } else {
+        //             elm.style.backgroundColor = arrCol[arrCol.length - 1];
+        //             // remove indexes in the arrays
+        //             arrItm.pop();
+        //             arrCol.pop();
+        //             // calls function again
+        //             back = setTimeout(bckWrds, 50);
+        //         }
+        //     }
+        // }
+    });
+
+    function bckWrds() {
+        console.log("bckWrds");
     }
 })();
 
